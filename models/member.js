86a56e0605360df06860fc8882cf7fe9788b5b01
H-b8 <carolinemv.le@gmail.com>
2020-10-02 22:06:55 -0700

    attempting URL refactor

--- models/member.js
+++ models/member.js
@@ -1,51 +1,51 @@
 const mongoose = require('mongoose');
 
 const linkSchema = new mongoose.Schema({
-  link: {
-    type: String,
-    required: true
-  },
-  linktext: {
-    type: String,
-    required: true
-  },
-  subtext: {
-    type: String,
-    default: 'SUBTEXT'
-  },
-  star: {
-    type: Boolean,
-    default: false
-  }
+	link: {
+		type: String,
+		required: true
+	},
+	linktext: {
+		type: String,
+		required: true
+	},
+	subtext: {
+		type: String,
+		default: 'SUBTEXT'
+	},
+	star: {
+		type: Boolean,
+		default: false
+	}
 }, {
-  timestamps: true
+	timestamps: true
 });
 
 const subSchema = new mongoose.Schema({
-  subEmail: {
-    type: String,
-    required: true
-  }
+	subEmail: {
+		type: String,
+		required: true
+	}
 }, {
-  timestamps: true
+	timestamps: true
 });
 
 const memberSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  googleId: String,
-  username: {
-    type: String,
-    default: 'username'
-  },
-  location: {
-    type: String,
-    default: 'location'
-  },
-  links: [linkSchema],
-  subscriptions: [subSchema]
+	name: String,
+	email: String,
+	googleId: String,
+	username: {
+		type: String,
+		default: 'username'
+	},
+	location: {
+		type: String,
+		default: 'location'
+	},
+	links: [linkSchema],
+	subscriptions: [subSchema]
 }, {
-  timestamps: true
+	timestamps: true
 });
 
 module.exports = mongoose.model('Member', memberSchema);
