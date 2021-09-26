d4d58aac77a67078b6d37a61c92636bd48b6aafc
CL <carolinemv.le@gmail.com>
2021-09-26 12:26:04 -0700

    upgrade heroku stack

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var indexRouter = require('./routes/index');
 var loginRouter = require('./routes/login');
 var memberRouter = require('./routes/member');
 
-// view engine setup
+// VIEW ENGINE SETUP
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -41,18 +41,18 @@ app.use('/', indexRouter);
 app.use('/login', loginRouter);
 app.use('/member', memberRouter);
 
-// catch 404 and forward to error handler
+// CATCH ERROR AND PASS TO HANDLER
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// HANDLE ERROR
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // SET LOCALS, PROVIDE ERRORS IN DEV ONLY
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // RENDER ERROR MESSAGE
   res.status(err.status || 500);
   res.render('error');
 });
